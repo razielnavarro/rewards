46e51c7857b8cd0a6ebb03d8cf6b23df1afcfaa4
razielnavarro <elrazand@gmail.com>
2025-04-02 12:37:55 -0500

    refactor: change start_date and end_date fields to use timestamp format in promotions entity

--- src/entities/promotions.entity.ts
+++ src/entities/promotions.entity.ts
@@ -12,8 +12,12 @@ export const promotions = sqliteTable('promotions', {
 	...baseEntityColumns,
 	title: text(),
 	description: text(),
-    start_date: text(),
-    end_date: text(),
+    start_date: integer("start_date", { mode: "timestamp" })
+    .default(sql`(strftime('%s', 'now'))`)
+    .notNull(),
+    end_date: integer("end_date", { mode: "timestamp" })
+    .default(sql`(strftime('%s', 'now'))`)
+    .notNull(),
 	type: text(),
 	is_premium: integer('is_premium', { mode: 'boolean' }).notNull().default(false),
 });
