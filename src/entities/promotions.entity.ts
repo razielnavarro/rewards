5ff5a80595338af4419289a43ad68d4a6f25e7e4
razielnavarro <elrazand@gmail.com>
2025-04-02 14:01:07 -0500

    feat: add promotions_category_id to promotions entity and establish relations with promotionsCategory

--- src/entities/promotions.entity.ts
+++ src/entities/promotions.entity.ts
@@ -1,23 +1,30 @@
 import { sql } from 'drizzle-orm';
 import { generateId } from '../utils/random';
 import { baseEntityColumns } from './base.entity';
+import { promotionsCategory } from './promotionsCategory.entity';
+import { relations } from 'drizzle-orm';
 
-import { 
-    sqliteTable, 
-    text, 
-    integer 
-} from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const promotions = sqliteTable('promotions', {
 	...baseEntityColumns,
 	title: text(),
 	description: text(),
-    start_date: integer("start_date", { mode: "timestamp" })
-    .default(sql`(strftime('%s', 'now'))`)
-    .notNull(),
-    end_date: integer("end_date", { mode: "timestamp" })
-    .default(sql`(strftime('%s', 'now'))`)
-    .notNull(),
-	type: text(),
+	start_date: integer('start_date', { mode: 'timestamp' })
+		.default(sql`(strftime('%s', 'now'))`)
+		.notNull(),
+	end_date: integer('end_date', { mode: 'timestamp' })
+		.default(sql`(strftime('%s', 'now'))`)
+		.notNull(),
+	promotion_category_id: text('promotion_category_id')
+		.notNull()
+		.references(() => promotionsCategory.id),
 	is_premium: integer('is_premium', { mode: 'boolean' }).notNull().default(false),
 });
+
+export const promotionsRelations = relations(promotions, ({ one }) => ({
+	category: one(promotionsCategory, {
+		fields: [promotions.promotion_category_id],
+		references: [promotionsCategory.id],
+	}),
+}));
--- src/entities/promotionsCategory.entity.ts
+++ src/entities/promotionsCategory.entity.ts
@@ -1,6 +1,8 @@
 import { sql } from 'drizzle-orm';
 import { generateId } from '../utils/random';
 import { baseEntityColumns } from './base.entity';
+import { promotions } from './promotions.entity';
+import { relations } from 'drizzle-orm';
 
 import { 
     sqliteTable, 
@@ -8,8 +10,12 @@ import {
     integer 
 } from 'drizzle-orm/sqlite-core';
 
-export const promotionCategory = sqliteTable('promotionCategory', {
+export const promotionsCategory = sqliteTable('promotionCategory', {
     ...baseEntityColumns,
 	is_multiplier: integer({ mode: 'boolean' }).notNull().default(false),
     amount: integer(),
 });
+
+export const promotionCategoryRelations = relations(promotionsCategory, ({ many }) => ({
+	promotions: many(promotions)
+}));
