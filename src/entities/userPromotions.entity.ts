436df1bd240707e6a6608ec640751c8c2edf07df
razielnavarro <elrazand@gmail.com>
2025-04-04 09:13:31 -0500

    feat: add foreign key relationship between userPromotions and promotions entities

--- src/entities/userPromotions.entity.ts
+++ src/entities/userPromotions.entity.ts
@@ -1,15 +1,22 @@
 import { sql } from 'drizzle-orm';
 import { generateId } from '../utils/random';
+import { promotions } from './promotions.entity';
+import { relations } from 'drizzle-orm';
 
-import { 
-    sqliteTable, 
-    text, 
-    integer 
-} from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 import { baseEntityColumns } from './base.entity';
 
 export const userPromotions = sqliteTable('userPromotions', {
-...baseEntityColumns,
-    user_id: text(),
-    promotion_id: text(),
+	...baseEntityColumns,
+	user_id: text(),
+	promotion_id: text()
+		.notNull()
+		.references(() => promotions.id),
 });
+
+export const userPromotionsRelations = relations(userPromotions, ({ one }) => ({
+    promotions: one(promotions, {
+        fields: [userPromotions.promotion_id],
+        references: [promotions.id],
+    }),
+}));
