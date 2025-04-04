0df2f055c4ff533f582f7454be1b704c13bfc8e5
razielnavarro <elrazand@gmail.com>
2025-04-04 10:02:40 -0500

    feat: update routing and main entry point in app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
-import { Hono } from "hono";
-import { Env } from "./common/types";
+import { Hono } from 'hono';
+import { Env } from './common/types';
 import {
-  historyController,
-  promotionTypeController,
-  promotionsController,
-  userPointsController,
-  userPromotionsController,
-
-} from "./controllers";
-import pointsController from "./controllers/points.controller";
+	historyController,
+	promotionTypeController,
+	promotionsController,
+	userBalanceController,
+	userPromotionsController,
+	pointsController,
+} from './controllers';
 
 const app = new Hono<Env>();
 
-app.route("/history", historyController);
-app.route("/promotions", promotionsController);
-app.route("/promotion-type", promotionTypeController);
-app.route("/user-points", userPointsController);
-app.route("/user-promotions", userPromotionsController);
-app.route("/points", pointsController);
+app.route('/history', historyController);
+app.route('/promotions', promotionsController);
+app.route('/promotion-type', promotionTypeController);
+app.route('/user-balance', userBalanceController);
+app.route('/user-promotions', userPromotionsController);
+app.route('/points', pointsController);
 
 export default app;
