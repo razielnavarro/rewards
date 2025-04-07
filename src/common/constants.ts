e10c4dfb9b34cda96ccdea0cc2c4c27acd64586a
razielnavarro <elrazand@gmail.com>
2025-04-07 15:22:22 -0500

    feat: add multiplier for converting points to dollars in constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,7 @@
 // Multiplier to convert dollars to points.
 // For example, if the value is 100, then $1 equals 100 points.
 export const DOLLARS_TO_POINTS_MULTIPLIER = 1;
+
+// Multiplier for converting points to dollars.
+// For example, if the value is 0.01, then 100 points equals $1.
 export const POINTS_TO_DOLLARS_MULTIPLIER = 0.01;
